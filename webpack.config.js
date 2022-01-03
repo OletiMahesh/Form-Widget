11e246245555cc5db1d1d7784193df777088e08c
Mahesh Oleti <moleti.ext@goeasy.com>
2022-01-03 23:51:54 +0530

    Changed docs to build

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'build'),
         libraryTarget: 'umd',
         library: 'form-widget'
     },

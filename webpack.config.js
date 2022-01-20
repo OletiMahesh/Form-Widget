43cbf88381aab7030a271091477d3f432f04f84b
Mahesh Oleti <moleti.ext@goeasy.com>
2022-01-21 00:06:20 +0530

    Modidified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -57,4 +58,7 @@ module.exports = {
         },
       ]
   },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 }

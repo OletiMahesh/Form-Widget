82f0e57b6e3d3ebad9a19f680c7150d8bf5bed74
Mahesh Oleti <moleti.ext@goeasy.com>
2022-02-08 16:31:29 +0530

    Changed code to expose window object

--- src/index.js
+++ src/index.js
@@ -6,17 +6,29 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Find all widget divs
-const WidgetDivs = document.querySelectorAll('.react_form_widget');
+// const WidgetDivs = document.querySelectorAll('.react_form_widget');
 
 // Inject our React App into each
-WidgetDivs.forEach(Div => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App domElement={Div} />
-    </React.StrictMode>,
-    Div
-  );
-})
+// WidgetDivs.forEach(Div => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <App domElement={Div} />
+//     </React.StrictMode>,
+//     Div
+//   );
+// })
+
+
+window.ReactCounter = {
+  mount: () => {
+    const el = document.getElementById('react_form_widget');
+    ReactDOM.render(<App domElement={el}/>, el);
+  },
+  unmount: () => {
+    const el = document.getElementById('react_form_widget');
+    ReactDOM.unmountComponentAtNode(el);
+  }
+}
 
 
 // If you want to start measuring performance in your app, pass a function
